8036969325f2fb09a42425bdf3dc2f8bb8ac8ee3
pedro.rodrigues29@fatec.sp.gov.br <PedroPereiraRodrigues>
2024-02-27 12:35:07 -0300

    Aula2702 - Componentes de React

---
+++ basic/basictools/src/Footer/Footer.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <>
+        <table>
+        <tr>
+            <tr>
+                Marca
+            </tr>
+            <tr>
+                Endereço
+            </tr>
+            <tr>
+                Contato
+            </tr>
+        </tr>
+        <tr>
+            <tr>React F3M</tr>
+            <tr>Rua de Educação, 120</tr>
+            <tr>f24@fatec.sp.gov.br</tr>
+        </tr>
+        </table>
+        </>
+    )
+}
